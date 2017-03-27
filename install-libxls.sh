efedc04ad1800f0d3385b47a460f1b86d0f333c8
Frank Xu <frank.xu@northbright.com>
2017-03-27 18:50:59 +0800

    Remove sudo requiremnt: install libxls to /home/xxu/lib

--- install-libxls.sh
+++ install-libxls.sh
@@ -3,6 +3,6 @@
 wget http://downloads.sourceforge.net/libxls/libxls-0.2.0.tar.gz
 tar -xzvf libxls-0.2.0.tar.gz
 cd libxls-0.2.0
-./configure
+./configure --prefix=$HOME
 make
-sudo make install
+make install
